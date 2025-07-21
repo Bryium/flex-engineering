d381d677005543fb889bfe390248d3d5c7fc9e75
Bryium <bryiumonyancha@gmail.com>
2025-07-21 18:30:32 +0300

    initial commit

---
+++ flex-engineering/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-blue-100 text-center p-6">
+      <h1 className="text-10xl font-bold text-blue-800">
+        Welcome to Flex Engineering 🚀
+      </h1>
+      <p className="text-lg text-blue-600 mt-4">Tailwind CSS is working!</p>
+    </div>
+  );
+}
+
+export default App;
